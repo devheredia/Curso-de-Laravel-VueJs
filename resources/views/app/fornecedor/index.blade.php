4d7a32beadfcd55193a128ad9de852fef8e543bc
devheredia <devheredia@gmail.com>
2024-05-03 16:39:48 -0300

    feat: 45. Blade - Incluíndo comentários e blocos PHP puros

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,11 @@
+<h3>Fornecedor</h3>
+
+{{-- Fica o comentário que será descartado pelo interpretador do blade.--}}
+
+@php
+    // Para comentários de uma linha.
+    /*
+        Para comentários de múltiplas linhas.
+    */
+    echo 'Texto de teste';
+@endphp
