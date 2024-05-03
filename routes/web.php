c9869b3b78b15830a05ed4a653d47976bd306765
devheredia <devheredia@gmail.com>
2024-05-03 10:10:28 -0300

    feat: Aula 36 - Criando o menu de navegação

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
+
+
 // Route::get(
 //             '/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
 //             function (
@@ -36,13 +38,13 @@ Route::get('/contato', 'ContatoController@contato');
 //                     }
 //         );
 
-Route::get(
-    '/contato/{nome}/{categoria_id}',
-    function (
-                string $nome = 'Desconhecido',
-                int $categoria_id = 1 // 1 = 'informação'
-            )
-            {
-                echo "Estamos aqui: $nome - $categoria_id";
-            }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+// Route::get(
+//     '/contato/{nome}/{categoria_id}',
+//     function (
+//                 string $nome = 'Desconhecido',
+//                 int $categoria_id = 1 // 1 = 'informação'
+//             )
+//             {
+//                 echo "Estamos aqui: $nome - $categoria_id";
+//             }
+// )->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
