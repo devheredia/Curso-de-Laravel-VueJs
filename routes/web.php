e4ec1b43075d6ffe764cf78c0e376f0565de808c
devheredia <devheredia@gmail.com>
2024-05-03 10:39:03 -0300

    feat: 38 - Agrupando Rotas

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,26 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get('/login', function(){return 'Login';});
+Route::prefix('/app')->group(function () {
 
-Route::get('/clientes', function(){return 'Clientes';});
+    Route::get('/login', function () {
+        return 'Login';
+    });
 
-Route::get('/fornecedores', function(){return 'Fornecedores';});
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    });
 
-Route::get('/produtos', function(){return 'Produtos';});
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    });
+
+    Route::get('/produtos', function () {
+        return 'Produtos';
+    });
+});
+
+
 
 
 
