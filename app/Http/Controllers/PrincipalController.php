92d91549715d50b6a5410577c9ea6ffe81e93880
devheredia <devheredia@gmail.com>
2024-05-18 20:07:17 -0300

    feat: :sparkles: 128. Ajustando o formulário de contato na rota principal

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -6,9 +6,14 @@ class PrincipalController extends Controller
 {
     public function principal()
     {
+        $motivo_contato = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
         // Você pode passar dados para a view aqui, se desejar
         // Por exemplo: $data = ['mensagem' => 'Bem-vindo ao site!'];
 
-        return view('site.principal');
+        return view('site.principal', ['motivo_contato' => $motivo_contato]);
     }
 }
