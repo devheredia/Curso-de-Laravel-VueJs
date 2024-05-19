ae9bded646b2f3dc36a406329886531e8ddf1571
devheredia <devheredia@gmail.com>
2024-05-19 01:09:55 -0300

    feat: :sparkles: 133. Validação de campos únicos (unique)

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -41,7 +41,7 @@ class ContatoController extends Controller
     {
         // dd($request);
         $request->validate([
-            'nome' => 'required|min:3|max:40',
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
