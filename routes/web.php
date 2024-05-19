7322d0153713ff78db971ce3af8c7d8de6bcda81
devheredia <devheredia@gmail.com>
2024-05-19 16:25:13 -0300

    feat: :sparkles: 138. Criando meu primeiro middleware

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\LogAcessoMiddleware;
 
 Route::fallback(function(){echo 'A rota acessada não existe. <a href="' . route('site.index') . '">Clique aqui</a>';});
-Route::get('/', 'App\Http\Controllers\PrincipalController@principal')->name('site.index');
+Route::get('/', 'App\Http\Controllers\PrincipalController@principal')->name('site.index')->middleware(LogAcessoMiddleware::class);
 Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'App\Http\Controllers\ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'App\Http\Controllers\ContatoController@salvar')->name('site.contato');
